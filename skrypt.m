eac57924f64e02dc263592834cc5e0c759c5cb2e
Urszula <u.bielinska@gmail.com>
2019-12-01 21:58:11 +0100

    Odbieranie sygnału c.d.

--- skrypt.m
+++ skrypt.m
@@ -4,23 +4,34 @@ dane=data_aq;
 load('data_el')
 a=data_aq;
 
+%filtracja sygna³u analogowego
 filtr1 = fir1(51, [13.45/50 17.45/50]);
 y = filter(filtr1, 1, data_aq);
 [up] = envelope(y, 5, 'rms');
 low = fir1(51, 2/50);   %filtr dolnoprzepustowy
 y = filter(low, 1, up);
 
-%c = abs(fft(y));
-%semilogy(c)
-%plot(y)
-pr = nrPRBS(11, 2047);
-prbss = prbs2(9,11,2047);
-us = resample(double(prbss), 50,1);
-% figure(1)
-% plot(pr)
-% hold on
-% plot(prbss)
+%odbiór sygna³u el
+dwn = downsample(a(38:end),50);
+nor = normalize(dwn);
+for i= 1:length(nor)
+    if nor(i) > 0
+        r(i) = 1;
+    else
+        r(i) = -1;
+    end
+end
+up2 = upsample(r, 50,1);
 
-y2=normalize(y,'range');
- plot(xcorr(us, y2))
+%usuniêcie przesuniêcia
+d = finddelay(up2,y);
+syg = circshift(up2, d);
+%op = finddelay(cs,y)
 
+y_nor=normalize(y);
+syg_nor = normalize(syg);
+
+figure()
+plot(syg_nor)
+hold on 
+plot(y_nor)
