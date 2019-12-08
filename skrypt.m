1e3eba856d609916d41794b4bb91e1dfbd556497
Piotr <araszkiewiczpiotr@gmail.com>
2019-12-08 02:27:30 +0100

    fft

--- skrypt.m
+++ skrypt.m
@@ -1,18 +1,31 @@
 clear all;
 load('data.mat')
-dane=data_aq;
+opdata=data_aq;
 load('data_el')
-a=data_aq;
+eldata=data_aq;
+clear data_aq;
 
 %filtracja sygna³u analogowego
 filtr1 = fir1(51, [13.45/50 17.45/50]);
-y = filter(filtr1, 1, data_aq);
+y = filter(filtr1, 1, opdata);
 [up] = envelope(y, 5, 'rms');
 low = fir1(51, 2/50);   %filtr dolnoprzepustowy
 y = filter(low, 1, up);
 
+%% eldata
+P2 = abs(fft(eldata)/length(eldata));
+P1 = P2(1:length(eldata)/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = 100e9*(0:(length(eldata)/2))/length(eldata);
+semilogy(f,P1) 
+title({'eldata'})
+xlabel('f (Hz)')
+ylabel('|eldata(f)|')
+xline(100e9/50,'-','2e9');
+%%
+
 %odbiór sygna³u el
-dwn = downsample(a(38:end),50);
+dwn = downsample(eldata(38:end),50);
 nor = normalize(dwn);
 for i= 1:length(nor)
     if nor(i) > 0
